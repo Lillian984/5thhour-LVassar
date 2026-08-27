1ae8ee9f478ac52bf9fcf85945a0df9bf15f82e7
Lila Vassar <lv129084@student.wpsok.org>
2026-08-27 12:52:42 -0500

    sUBMITTED homework 3

---
+++ HW3.py
@@ -0,0 +1,48 @@
+
+#Name:
+#Class: 5th Hour
+#Assignment: HW3
+
+
+#1. Print "Hello World!"
+
+print("Hello world")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+
+Candy = "Skittles"
+My_money_amount = 68
+I_am_normal = False
+
+#3. Print all three variables on the same print function (at the same time).
+
+print(Candy,My_money_amount,I_am_normal)
+
+#4. Create a variable that asks the user to input an integer.
+
+enter_int = int(input("Enter an integer:"))
+
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+
+rainbow = (My_money_amount+enter_int)
+print(rainbow)
+
+#6. Take the result from #5 and divide it by 2. Print the result.
+
+print(rainbow/2)
+
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+
+I_am_normal = True
+
+#8. Print the value of the boolean variable.
+
+print(I_am_normal)
+
+#9. Create a variable with a number that contains decimals.
+
+Tate = 2.8162
+
+#10. Round the number from #9 up or down using the round function.
+
+print(round(Tate))
